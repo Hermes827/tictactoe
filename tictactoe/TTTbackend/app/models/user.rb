82fb5cfe79f844a2d4f36504a8604368fdecd0b1
Chris Cable <ccable827@gmail.com>
2020-06-19 13:43:29 -0700

    moved folders

--- tictactoe/TTTbackend/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  # attr_accessor :email, :password
-end
-
-# has_secure_password is causing the bug and not letting data persist in the db
-# with only has_secure_password enabled the bcrypt works when I make something in the console
